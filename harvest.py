19069e7757250c5664e94d73d1a495b631a12b5a
shoshlavosh <shoshlavosh@gmail.com>
2021-07-22 19:59:40 +0000

    Prtially completed but not working

--- harvest.py
+++ harvest.py
@@ -6,23 +6,28 @@
 class MelonType(object):
     """A species of melon at a melon farm."""
 
-    def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, 
-                 name):
+    def __init__(self, name, code, first_harvest, color, is_seedless, is_bestseller):
         """Initialize a melon."""
 
         self.pairings = []
 
-        # Fill in the rest
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless 
+        self.is_bestseller = is_bestseller
+        self.name = name
+
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
 
-        # Fill in the rest
+        self.pairings.append(pairing)
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
-        # Fill in the rest
+        self.code = new_code
 
 
 def make_melon_types():
@@ -30,14 +35,28 @@ def make_melon_types():
 
     all_melon_types = []
 
-    # Fill in the rest
+    def __init__(self, all_melon_types):
+        """Return all melon types"""
+
+        musk = MelonType(
+            "musk",
+            "Muskmelon",
+            1998,
+            "green",
+            True,
+            True
+        )
+        musk.add_pairing("mint")
+
+        all_melon_types.append(musk)
 
     return all_melon_types
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
-    # Fill in the rest
+    for melon in melon_types:
+        print(f"{name} pairs with /n - {pairing}")
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
